64a1123f06bf9d7757dce713841c69e025ed52ed
Sam Steinberg <ssteinb2@vols.utk.edu>
2019-03-13 16:12:00 -0400

    add features to make and .sh

--- project4/run.sh
+++ project4/run.sh
@@ -1,3 +1,14 @@
+# Samuel Steinberg
+# March 13th, 2019
+# CS420 Project 4 
+
+
 #!/bin/bash
 
+set -e  # stop script at first error
+
+make -f makefile
+
 ./GenAlgorithm 20 30 10 0.033 0.6 5
+
+echo Simulation Complete
